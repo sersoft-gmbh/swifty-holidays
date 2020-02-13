d5a92f5e09e14332a6d44d92403e24f47717f350
Florian Friedrich <ffried@me.com>
2020-02-13 16:27:41 +0100

    Add another test

---
+++ Tests/SwiftyHolidaysTests/CalculatorTests.swift
@@ -0,0 +1,27 @@
+import XCTest
+@testable import SwiftyHolidays
+
+final class CalculatorTests: XCTestCase {
+    fileprivate struct MockCalc: Calculator {
+        struct Context: CalculationContext {
+            mutating func merge(with other: Context) {}
+            mutating func clear() {}
+        }
+        let calendar: Calendar = {
+            var cal = Calendar(identifier: .gregorian)
+            cal.timeZone = TimeZone(secondsFromGMT: 0)!
+            return cal
+        }()
+
+        var context = Context()
+
+        mutating func initialize(with context: Context) { self.context = context }
+    }
+    
+    func testDateCreation() {
+        let calculator = MockCalc()
+        let date = TimelessDate(day: 29, month: 08, year: 2019)
+        XCTAssertEqual(calculator.date(for: date, atNoon: true), Date(timeIntervalSinceReferenceDate: 588772800))
+        XCTAssertEqual(calculator.date(for: date, atNoon: false), Date(timeIntervalSinceReferenceDate: 588729600))
+    }
+}
